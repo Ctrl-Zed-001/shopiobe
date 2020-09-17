da23fa88aea26dd8602b63055a6edae1ff8e0c16
zed <zshake11@gmail.com>
2020-09-17 10:10:05 +0530

    recieve pending amount API done

--- API/Order.js
+++ API/Order.js
@@ -2,6 +2,13 @@ const express = require("express")
 const router = express.Router()
 const Order = require("../Models/Order")
 const Product = require("../Models/Product")
+// const escpos = require('escpos');
+// // install escpos-usb adapter module manually
+// escpos.USB = require('escpos-usb');
+// // Select the adapter based on your printer type
+// const device = new escpos.USB();
+
+// const printer = new escpos.Printer(device, options);
 
 router.get("/all", (req, res, next) => {
     Order.find()
@@ -58,7 +65,7 @@ router.post("/new", (req, res, next) => {
         grandtotal: req.body.grandtotal,
         tax: req.body.tax,
         discount: req.body.discount,
-        recieved: req.body.recieved,
+        recieved: parseInt(req.body.recieved),
         pending: req.body.pending,
         paymentmode: req.body.paymentmode,
         paymentstatus: req.body.paymentstatus,
@@ -67,6 +74,27 @@ router.post("/new", (req, res, next) => {
 
     newOrder.save()
         .then(doc => {
+            // PRINT RECIEPT
+            // device.open(function (error) {
+            //     printer
+            //         .font('a')
+            //         .align('ct')
+            //         .style('bu')
+            //         .size(1, 1)
+            //         .text('The quick brown fox jumps over the lazy dog')
+            //         .barcode('1234567', 'EAN8')
+            //         .table(["One", "Two", "Three"])
+            //         .tableCustom(
+            //             [
+            //                 { text: "Left", align: "LEFT", width: 0.33, style: 'B' },
+            //                 { text: "Center", align: "CENTER", width: 0.33 },
+            //                 { text: "Right", align: "RIGHT", width: 0.33 }
+            //             ],
+            //             { encoding: 'cp857', size: [1, 1] } // Optional
+            //         )
+
+            // });
+
             res.status(201).send(doc)
         })
         .catch(err => {
@@ -80,6 +108,26 @@ router.get("/byuser/:id", (req, res, next) => {
             res.status(200).send(doc)
         })
         .catch(err => {
+            res.status(500).send(err)
+        })
+})
+
+router.post("/update_pending", (req, res, next) => {
+    Order.findById(req.body.orderid)
+        .then(doc => {
+            doc.recieved += parseInt(req.body.amount);
+            doc.pending -= parseInt(req.body.amount);
+            if (doc.pending == 0) {
+                doc.paymentstatus = "Completed";
+            }
+            doc.save()
+                .then(newdoc => {
+                    res.status(200).json({ status: "updated" })
+                })
+                .catch(error => {
+                    res.status(500).send(error)
+                })
+        }).catch(err => {
             res.status(500).send(err)
         })
 })
