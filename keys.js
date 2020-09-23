c184f87ef574f09ee387c32171a22aa9f9764548
Zaid Shaikh <zshake11@gmail.com>
2020-09-23 20:41:46 +0530

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,5 @@
 module.exports = {
     secret: "Kaioken*10",
     url: "https://shopio.herokuapp.com/",
-    localmongourl: "mongodb://localhost:27017/shopio",
-    mongourl: "mongodb+srv://zed:Kaioken*2@cluster0-jiwvj.mongodb.net/shopio?retryWrites=true&w=majority"
+    mongourl: "mongodb://localhost:27017/shopio",
 }
